f3f775da25729d34883811bcfa0a055d49ab5eab
BeeCashG <gbikash34@gmail.com>
2022-05-07 17:50:59 +0545

    presentation and proposal

--- gharbeti/lib/main.dart
+++ gharbeti/lib/main.dart
@@ -1,5 +1,8 @@
 // ignore_for_file: unnecessary_const
 
+import 'package:anim_search_bar/anim_search_bar.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -51,55 +54,170 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
-    //
+    double W = MediaQuery.of(context).size.width;
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        // leading: Text(widget.title,
-        //     style: const TextStyle(color: Colors.green, fontSize: 26.0),
-        //     textAlign: TextAlign.left),
-        title: const Text("GharBeti"),
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.lightGreen,
-        elevation: 0,
-        leadingWidth: 150.0,
-        actions: const [
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Icon(Icons.home),
-          )
-        ],
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end:
+              Alignment(0.8, 0.0), // 10% of the width, so there are ten blinds.
+          colors: <Color>[Color(0xffffffff), Color(0xffffffff)],
+        ),
       ),
-      body: SingleChildScrollView(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            SizedBox(
-              width: 20.0,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          // leading: Text(widget.title,
+          //     style: const TextStyle(color: Colors.green, fontSize: 26.0),
+          //     textAlign: TextAlign.left),
+          title: SizedBox(
+            width: 250.0,
+            child: AnimatedTextKit(
+              animatedTexts: [
+                TypewriterAnimatedText(
+                  'GharBeti',
+                  textStyle: const TextStyle(
+                      color: Color.fromARGB(255, 27, 218, 125),
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+              repeatForever: true,
             ),
-            SizedBox(
-              height: 50.0,
-              width: 400.0,
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(width: 1),
-                      borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                    ),
-                    hintText: "Search Room, Flat, Appartment, House",
-                    suffixIcon: Icon(Icons.search)),
+          ),
+          backgroundColor: Colors.transparent,
+          foregroundColor: const Color.fromARGB(255, 0, 0, 0),
+          elevation: 0,
+          leadingWidth: 150.0,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: AnimSearchBar(
+                width: 400,
+                textController: textController,
+                rtl: true,
+                helpText: "Search Room, Flat, Appartment, House...",
+                onSuffixTap: () {
+                  setState(() {
+                    textController.clear();
+                  });
+                },
               ),
-            ),
+            )
           ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 10.0),
+                CarouselSlider(
+                  options: CarouselOptions(
+                    autoPlay: true,
+                    aspectRatio: 5,
+                  ),
+                  items: [
+                    Container(
+                      color: Color.fromRGBO(255, 87, 34, 1),
+                      child: SizedBox(
+                        width: W,
+                      ),
+                    ),
+                    Container(
+                      color: Color.fromARGB(255, 34, 255, 82),
+                      child: SizedBox(
+                        width: W,
+                      ),
+                    ),
+                    Container(
+                      color: Color.fromARGB(255, 34, 13, 222),
+                      child: SizedBox(
+                        width: W,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                RichText(
+                  text: const TextSpan(
+                    // Note: Styles for TextSpans must be explicitly defined.
+                    // Child text spans will inherit styles from parent
+                    style: const TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.black,
+                    ),
+                    children: <TextSpan>[
+                      const TextSpan(
+                          text: 'Explore',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 0, 0, 0),
+                              fontWeight: FontWeight.bold)),
+                      const TextSpan(
+                          text: ' GharBeti',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 27, 218, 125),
+                              fontWeight: FontWeight.bold)),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                const SizedBox(height: 10.0),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(children: [
+                    Container(
+                      height: 100,
+                      width: 200,
+                      color: Colors.lightGreenAccent,
+                      child: Text("Data"),
+                    ),
+                    const SizedBox(width: 10.0),
+                    Container(
+                      color: Colors.lightGreenAccent,
+                      height: 100,
+                      width: 200,
+                      child: Text("Data"),
+                    ),
+                    const SizedBox(width: 10.0),
+                    Container(
+                      color: Colors.lightGreenAccent,
+                      height: 100,
+                      width: 200,
+                      child: Text("Data"),
+                    ),
+                    const SizedBox(width: 10.0),
+                    Container(
+                      color: Colors.lightGreenAccent,
+                      height: 100,
+                      width: 200,
+                      child: Text("Data"),
+                    ),
+                    const SizedBox(width: 10.0),
+                    Container(
+                      color: Colors.lightGreenAccent,
+                      height: 100,
+                      width: 200,
+                      child: Text("Data"),
+                    ),
+                  ]),
+                )
+              ]),
         ),
       ),
     );
