1899f6df747683437b5d395fcd1c3030aa128b69
BeeCashG <gbikash34@gmail.com>
2022-08-21 22:22:40 +0545

    backend update

---
+++ backend/src/api/rental-detail/controllers/rental-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  rental-detail controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::rental-detail.rental-detail');
