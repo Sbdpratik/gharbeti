e0a466dcf7c6019f7bd2e3723b8d1b2246e40574
laxmiGajurel <107293512+laxmiGajurel@users.noreply.github.com>
2022-08-21 22:25:58 +0545

    backend change

---
+++ backend/src/api/rental-detail/routes/rental-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * rental-detail router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::rental-detail.rental-detail');
---
+++ backend/src/api/rental-detail/services/rental-detail.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * rental-detail service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::rental-detail.rental-detail');
